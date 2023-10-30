6613d46d1f8d31eb6205a866f152f6f183c4c49a
Clovis <clovissantannaa@gmail.com>
2023-10-30 19:43:19 -0300

    Adjusted

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function Home(){
     return(
         <View>
-            <Text>Hello word</Text>
+            <Text>Hello word!!!</Text>
         </View>
     )
 }
